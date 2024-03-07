63eb0dbdca275d06b1df20b4a5890fe5fddc0452
m <mbddvm@gmail.com>
2024-03-07 07:04:17 -0600

    Delete build

--- build/routes/dogRouter.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-import { getAllDogs, getDog, createDog, editDog, deleteDog, } from '../controllers/dogController.js';
-
-router.route('/').get(getAllDogs).post(createDog);
-router.route('/:id').get(getDog).patch(editDog).delete(deleteDog);
-export default router;
